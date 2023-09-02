d2549ed0bd46e21e58ddd35ce5ab1b49fc5d01f4
mii9000 <ibrahim.islam@live.com>
2023-09-03 00:54:45 +0600

    refactors and feature to parse table data

--- index.js
+++ index.js
@@ -1,6 +1,122 @@
-import keyValueParser from './key-value-pair-parser.js';
+const getKvRelationship = (keyMap, valueMap, blockMap) => {
+	const kvs = {};
 
-export const getKeyValuePairs = blocks => {
-	const result = keyValueParser(blocks);
-	return result;
+	for (const [, keyBlock] of Object.entries(keyMap)) {
+		const valueBlock = findValueBlock(keyBlock, valueMap);
+		const key = getText(keyBlock, blockMap);
+		const value = getText(valueBlock, blockMap);
+		kvs[key] = value;
+	}
+
+	return kvs;
 };
+
+const findValueBlock = (keyBlock, valueMap) => {
+	let valueBlock;
+
+	for (let i = 0; i < keyBlock.Relationships.length; i++) {
+		const relationship = keyBlock.Relationships[i];
+		if (relationship.Type === 'VALUE') {
+			for (let j = 0; j < relationship.Ids.length; j++) {
+				const valueId = relationship.Ids[j];
+				valueBlock = valueMap[valueId];
+			}
+		}
+	}
+
+	return valueBlock;
+};
+
+const getText = (result, blocksMap) => {
+	let text = '';
+
+	if (result.Relationships) {
+		for (let i = 0; i < result.Relationships.length; i++) {
+			const relationship = result.Relationships[i];
+			if (relationship.Type === 'CHILD') {
+				for (let j = 0; j < relationship.Ids.length; j++) {
+					const childId = relationship.Ids[j];
+					const word = blocksMap[childId];
+					if (word.BlockType === 'WORD') {
+						text += `${word.Text} `;
+					}
+
+					if (
+						word.BlockType === 'SELECTION_ELEMENT'
+						&& word.SelectionStatus === 'SELECTED'
+					) {
+						text += 'X ';
+					}
+				}
+			}
+		}
+	}
+
+	return text.trim();
+};
+
+const extractTableData = (cellMap, tableMap, blockMap) => {
+	// Process Tables
+	const tables = [];
+	for (const tableId in tableMap) {
+		const tableBlock = tableMap[tableId];
+		const rows = [];
+
+		for (const relationship of tableBlock.Relationships) {
+			if (relationship.Type === 'CHILD') {
+				for (const cellId of relationship.Ids) {
+					const cellBlock = cellMap[cellId];
+					const rowIndex = cellBlock.RowIndex;
+					const colIndex = cellBlock.ColumnIndex;
+					const text = getText(cellBlock, blockMap);
+
+					// Ensure a list exists for the row, then insert the cell text
+					rows[rowIndex] = rows[rowIndex] || [];
+					rows[rowIndex][colIndex] = text.trim();
+				}
+			}
+		}
+
+		tables.push(rows);
+	}
+
+	return tables;
+};
+
+const extract = blocks => {
+	const keyMap = {};
+	const valueMap = {};
+	const blockMap = {};
+	const cellMap = {};
+	const tableMap = {};
+
+	for (const block of blocks) {
+		const blockId = block.Id;
+		blockMap[blockId] = block;
+
+		if (block.BlockType === 'KEY_VALUE_SET') {
+			block.EntityTypes.includes('KEY')
+				? (keyMap[blockId] = block)
+				: (valueMap[blockId] = block);
+		}
+
+		if (block.BlockType === 'CELL') {
+			cellMap[blockId] = block;
+		}
+
+		if (block.BlockType === 'TABLE') {
+			tableMap[blockId] = block;
+		}
+	}
+
+	const pairs = getKvRelationship(keyMap, valueMap, blockMap);
+	const tables = extractTableData(cellMap, tableMap, blockMap);
+
+	return {pairs, tables};
+};
+
+const extractor = {
+	extract,
+};
+
+export default extractor;
--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 import {readFileSync} from 'node:fs';
 import test from 'ava';
-import {getKeyValuePairs} from './index.js';
+import extractor from './index.js';
 
-test('getKeyValuePairs', t => {
-	// Arrange
-	const contents = readFileSync('test-block-response.json', 'utf8');
-	const blocks = JSON.parse(contents);
+// Arrange
+const contents = readFileSync('test-block-response.json', 'utf8');
+const blocks = JSON.parse(contents);
 
+test('key-value pairs', t => {
 	// Act
-	const result = getKeyValuePairs(blocks);
+	const result = extractor.extract(blocks).pairs;
 
 	// Assert
 	t.assert(result['INVOICE NUMBER:'] === 'ABC123');
@@ -18,4 +18,17 @@ test('getKeyValuePairs', t => {
 	t.assert(result['TAX RATE'] === '5%');
 	t.assert(result.TAX === '$0.76');
 	t.assert(result['INVOICE TOTAL'] === '$14.36');
+});
+
+test('table', t => {
+	// Act
+	const result = extractor.extract(blocks).tables;
+
+	// Assert
+	t.assert(result.length === 1, 'Should be only 1 table');
+	t.assert(result[0].length > 12, 'Should have at least 12 rows');
+	const targetRow = result[0].find(row => row && row.includes('Trouser'));
+	t.assert(targetRow && targetRow.includes('$2.00'), 'Should match unit price');
+	t.assert(targetRow && targetRow.includes('$6.00'), 'Should match total price');
+	t.assert(targetRow && targetRow.includes('3'), 'Should match quantity');
 });
